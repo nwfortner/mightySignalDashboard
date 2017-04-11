6d2b2ccf938d14155b4900e7769b5dae23666dc9
Nicholas W Fortner <nwfortner@gmail.com>
2017-04-11 16:10:32 -0700

    [modify](db) config for process.env.dburi use

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -2,24 +2,12 @@ const bluebird = require('bluebird');
 const monitor = require('pg-monitor');
 console.log(process.env.DATABASE_URL);
 
-let dbConfiguration;
-
-if(process.env.DATABASE_URL) {
-  dbConfiguration = {
-    host: 'ec2-23-23-227-188.compute-1.amazonaws.com',
-    port: '5432',
-    databse: 'delgp3pe84hsln',
-    user: 'sowcrtcbrpmjsq',
-    password: '1ee58d140f90a95690c7d6ae1236caa8a6b52a13d7d644ce3745bd5af3f2c721'
-  };
-}else {
-  dbConfiguration = {
-    host:'localhost',
-    port: '5432',
-    database: 'mightysignaldb',
-    ssl: true
-  };
-}
+const dbConfiguration = process.env.DATABASE_URL || {
+  host:'localhost',
+  port: '5432',
+  database: 'mightysignaldb',
+  ssl: true
+};
 
 const options = {
   promiseLib: bluebird
